1115d4caba8918adc36aefa98e25fe5e4744ffd7
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-07-19 10:38:19 +0200

    Added validation error view.

--- app/Http/Livewire/PackerCard.php
+++ app/Http/Livewire/PackerCard.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Http\Controllers;
+use App\Http\Controllers\Articles;
+
 
 class PackerCard extends Component
 {
@@ -13,9 +15,15 @@ class PackerCard extends Component
     public $depth;
     public $boxSize;
     public $articleNumber;
+   
+    
 
     public function render()
     {
-        return view('livewire.packer-card');
+       
+        return view('livewire.packer-card');   
     }
+
+   
+
 }
